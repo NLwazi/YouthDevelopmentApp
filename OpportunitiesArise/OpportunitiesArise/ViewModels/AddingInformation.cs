312f6c1adfc6bf9eb84e161a3460c667ed3bb7a0
Lwazi Nhleko <nlwazi@ymail.com>
2021-03-18 12:21:54 +0200

    Adding Map Services

---
+++ OpportunitiesArise/OpportunitiesArise/ViewModels/AddingInformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpportunitiesArise.ViewModels
+{
+    public class AddingInformation
+    {
+
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string LabelName { get; } = "Let's get to\nknow each\nother";
+        public string BodyParagraph { get; } = "By joining you stand a chance to obtain a new skill and have greater chances of landing a new job\nWe are looking forward to the future";
+
+
+
+
+    }
+}
